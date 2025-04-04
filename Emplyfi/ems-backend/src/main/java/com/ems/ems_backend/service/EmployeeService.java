622e283f27780fc71a8a706c82388353708acc8f
Varad Mule <varadmule17@gmail.com>
2025-04-04 22:25:34 +0530

    Backend done

--- Emplyfi/ems-backend/src/main/java/com/ems/ems_backend/service/EmployeeService.java
+++ Emplyfi/ems-backend/src/main/java/com/ems/ems_backend/service/EmployeeService.java
@@ -1,9 +1,24 @@
 package com.ems.ems_backend.service;
 
+import java.util.List;
+
 import com.ems.ems_backend.dto.EmployeeDto;
 
 public interface EmployeeService {
 
+	// Create
 	EmployeeDto createEmployee(EmployeeDto employeeDto);
 
+	// Get employee
+	EmployeeDto getEmployeeById(Long employeeId);
+
+	// Get all employees
+	List<EmployeeDto> getAllEmployees();
+
+	// Update employee
+	EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmployee);
+	
+	// Delete Employee
+	void deleteEmployee(Long employeeId);
+	
 }
--- Emplyfi/ems-backend/src/main/java/com/ems/ems_backend/service/impl/EmployeeServiceImpl.java
+++ Emplyfi/ems-backend/src/main/java/com/ems/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,12 @@
 package com.ems.ems_backend.service.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import com.ems.ems_backend.dto.EmployeeDto;
 import com.ems.ems_backend.entity.Employee;
+import com.ems.ems_backend.exception.ResourceNotFoundException;
 import com.ems.ems_backend.mapper.EmployeeMapper;
 import com.ems.ems_backend.repository.EmployeeRepository;
 import com.ems.ems_backend.service.EmployeeService;
@@ -10,21 +14,66 @@ import com.ems.ems_backend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 
 @Service
-@AllArgsConstructor 
+@AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
 	private EmployeeRepository employeeRepository;
 
+	// Create Employee
 	@Override
 	public EmployeeDto createEmployee(EmployeeDto employeeDto) {
 
 		// Convert EmployeeDto (received from frontend) to Employee entity
 		Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
-		
+
 		// Save the Employee entity into the database using JPA's save() method
 		Employee savedEmployee = employeeRepository.save(employee);
-		
+
 		// Convert the saved Employee entity back to EmployeeDto and return it
 		return EmployeeMapper.mapToEmployeeDto(savedEmployee);
 	}
+
+	// Get Employee
+	@Override
+	public EmployeeDto getEmployeeById(Long employeeId) {
+
+		Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+				() -> new ResourceNotFoundException("Employee does not exists with given id : " + employeeId));
+
+		return EmployeeMapper.mapToEmployeeDto(employee);
+	}
+
+	// Get All Employee
+	@Override
+	public List<EmployeeDto> getAllEmployees() {
+		List<Employee> employees = employeeRepository.findAll();
+		return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+				.collect(Collectors.toList());
+	}
+
+	// Update Employee
+	@Override
+	public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmployee) {
+		Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+				() -> new ResourceNotFoundException("Employee does not exists with given id: " + employeeId));
+
+		employee.setFirstName(updateEmployee.getFirstName());
+		employee.setLastName(updateEmployee.getLastName());
+		employee.setEmail(updateEmployee.getEmail());
+
+		Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+		return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+	}
+
+	@Override
+	public void deleteEmployee(Long employeeId) {
+
+		Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+				() -> new ResourceNotFoundException("Employee does not exists with given id : " + employeeId));
+
+		employeeRepository.deleteById(employeeId);
+		
+	}
+
 }
