aab66e6aecab1af03b19270e0948eaf762629f92
Varad Mule <varadmule17@gmail.com>
2025-04-11 19:22:54 +0530

    department service interface create method added

---
+++ Emplyfi/ems-backend/src/main/java/com/ems/ems_backend/service/DepartmentService.java
@@ -0,0 +1,9 @@
+package com.ems.ems_backend.service;
+
+import com.ems.ems_backend.dto.DepartmentDto;
+
+public interface DepartmentService{
+	
+	// create 
+	public DepartmentDto createDepartment(DepartmentDto departmentdto) ;
+}
